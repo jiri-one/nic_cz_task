ed990c5a9ebcaea99a54214e44f6db36e3373cd6
jiri.one <nemec@jiri.one>
2022-06-21 12:33:33 +0200

    finished file-client

--- task_python_backend_2022/python/file-client.py
+++ task_python_backend_2022/python/file-client.py
@@ -1,29 +1,53 @@
+from os import write
+from shutil import which
 import requests
 import click
 
 SERVER = "localhost:8000" # adress without http:// and with port if is not 80
 
-def get_file_stats(server, file):    
+def stat(server, UUID):
     try:
-        resp = requests.get(f"http://{server}/file/{file}/stat/")
+        resp = requests.get(f"http://{server}/file/{UUID}/stat/")
     except requests.ConnectionError:
         print("Connection error")
-    data = resp.json()
-    return f"""
-    Information about file:\n
-    File name is: {data["name"]}\n
-    File size in bytes is: {data["size"]}\n
-    File mimetype is: {data["mimetype"]}\n
-    File was created at: {data["create_datetime"]}\n
-    """
+    if resp.status_code == 200:
+        data = resp.json()
+        return f"""
+        Information about file:
+        File name is: {data["name"]}
+        File size in bytes is: {data["size"]}
+        File mimetype is: {data["mimetype"]}
+        File was created at (different on every OS): {data["create_datetime"]}\n
+        """
+    else:
+        print(resp.reason)
 
-def get_file_bytes(server, file):
+def read(server, UUID):
     try:
-        resp = requests.get(f"http://{server}/file/{file}/read/")
+        resp = requests.get(f"http://{server}/file/{UUID}/read/")
     except requests.ConnectionError:
         print("Connection error")
     return resp.content
 
+@click.command()
+@click.argument('method', type=click.Choice(['stat', 'read']))
+@click.argument('UUID')
+@click.option('--backend', "backend", help="Set a backend to be used, choices are grpc and rest. Default is grpc.", default="grpc")
+@click.option('--grpc-server', "grpc_server", help="Set a host and port of the gRPC server. Default is localhost:50051.", default="localhost:50051")
+@click.option('--base-url', "base_url", help="Set a base URL for a REST server. Default is localhost:8000", default="localhost:8000")
+@click.option('--output', "output", help="Set the file where to store the output. Default is the stdout.", default="stdout")
+def cli(backend, grpc_server, base_url, output, method, uuid):
+    if backend == "rest":
+        if method == "stat":
+            print(stat(base_url, uuid))
+        elif method == "read":
+            if output == "stdout":
+                print(read(base_url, uuid))
+            else:
+                with open(output, "wb") as f:
+                    f.write(read(base_url, uuid))
+    else:
+        print("Not implemented yet")
 
-
-print(get_file_bytes(SERVER, "10fe4a26-e33d-4856-917d-32e7e703c97b"))
+if __name__ == '__main__':
+    cli()
